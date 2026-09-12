3cfdcf97ebd6f9edaacc87be95744f63bd481245
Sunday-Co <hello@sundayandcompany.co>
2026-09-12 04:08:56 +0000

    Version final shared assets and enable hero motion

--- assets/site.js
+++ assets/site.js
@@ -12,7 +12,7 @@
     if (document.querySelector('link[data-sunday-rendered-corrections]')) return;
     var link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = '/assets/rendered-corrections.css?v=20260912-1';
+    link.href = '/assets/rendered-corrections.css?v=20260912-4';
     link.setAttribute('data-sunday-rendered-corrections', 'true');
     document.head.appendChild(link);
   }
